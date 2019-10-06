f7e97d1a9f749a20337025af8e4b743f94aaa7af
Merculous <pettit.brandon12@gmail.com>
2019-10-05 21:48:26 -0700

    Change quotes, also update format strings

--- grabkeys.py
+++ grabkeys.py
@@ -7,14 +7,13 @@ from clint.textui import progress
 #TODO Have user enter pwndfu, enter ipsw path, extract files, import modules from ipwndfu to decrypt with gid key.
 
 def downloadIPSW(device, version):
-        # Adding "/buildid" returns the buildid when we search with iOS version
-        url = 'https://api.ipsw.me/v3/' + device + '/' + version + '/buildid' # Hot fix by my friend 32-bites in PyKeys
+        url = f'https://api.ipsw.me/v3/{device}/{version}/buildid'
         req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         data = urlopen(req).read()
         buildid = data.decode() # Not sure if we need to decode this.
         print(buildid)
 
-        realurl = 'https://api.ipsw.me/v4/ipsw/download/' + device + '/' + buildid
+        realurl = f'https://api.ipsw.me/v4/ipsw/download/{device}/{buildid}'
         print(realurl)
 
         #TODO Download the ipsw :/ Apparently I'm too dumb right now to get this working lol
@@ -24,26 +23,27 @@ def downloadIPSW(device, version):
 def extractIPSW(file):
         if zipfile.is_zipfile(file) == True:
                 tmp = tempfile.mkdtemp() # Make temp dir
-                print("We are using %s as the temp dir" % tmp)
+                print(f'We are using {tmp} as the temp dir')
                 with zipfile.ZipFile(file, 'r') as ipsw:
                         ipsw.extractall(tmp)
 
         else:
-                print("%s is not a zip archive" % file)
+                print(f'{file} is not a zip archive')
 
 def splitKbag(str):
-        if len(str) != 96:
-                print("Length: %d" % len(str))
-                sys.exit("String provided is not 96 bytes!")
+        size = len(str)
+        if size != 96:
+                print(f'Length: {size}')
+                sys.exit('String provided is not 96 bytes!')
         else:
                 #TODO I know for sure this can be made better. Maybe?
                 iv = str[:32]
                 key = str[-64:]
-                print("IV: %s" % iv)
-                print("Key: %s" % key)
+                print(f'IV: {iv}')
+                print(f'Key: {key}')
 
 def usage(name):
-        print('Usage: %s <args> <stuff>' % name)
+        print(f'Usage: {name} <args> <stuff>')
         print('-d\t\tDownload an ipsw for decrypting')
         print('-s\t\tSplit KBAG key')
         print('-x\t\tExtract IPSW')
@@ -54,7 +54,7 @@ if __name__ == '__main__':
                 usage(argv[0])
 
         if len(argv) > 4:
-                sys.exit("We are expecting two arguments but a max of 3!")
+                sys.exit('We are expecting two arguments but a max of 3!')
 
         if '-d' in argv:
                 downloadIPSW(argv[2], argv[3]) # ./grabkeys.py -d device version
