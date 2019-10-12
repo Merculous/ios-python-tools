027442d6906d2f549deb12f065250b95b53a1196
Merculous <pettit.brandon12@gmail.com>
2019-10-11 18:46:36 -0700

    Still working on this, I'm a noob

--- api.py
+++ api.py
@@ -1,20 +1,29 @@
+import json
 from urllib.parse import urlsplit
 from urllib.request import urlopen, Request, urlretrieve
 
 
-def downloadIPSW(device, version):
+def versionToBuildid(device, version):
     url = f'https://api.ipsw.me/v3/{device}/{version}/buildid'
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    data = urlopen(req).read()
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0', 'Accept': 'application/json'})
+    data = urlopen(req).read()  # Throws an exception if there are more than one buildid's for an iOS, json is needed?
     buildid = data.decode()  # Must be decoded
     print(buildid)
+    # TODO Some how return (print also for debugging for now) the buildid but also use that for downloadIPSW.
 
-    tmpurl = f'https://api.ipsw.me/v4/ipsw/download/{device}/{buildid}'
-    print(tmpurl)
 
-    realurl = urlopen(tmpurl).geturl()
-    print(realurl)
-
+def downloadIPSW(device, buildid):
+    tmpurl = f'https://api.ipsw.me/v4/ipsw/download/{device}/{buildid}'  # This will point to the api.ipsw.me url only
+    realurl = urlopen(tmpurl).geturl()  # This will give the actual Apple ipsw url
     split = urlsplit(realurl)
-    filename = split.path.split('/')[-1]
+    filename = split.path.split('/')[-1]  # This just grabs the name of the ipsw from the Apple url
     urlretrieve(realurl, filename)
+
+
+def grabKeys(device, version):
+    url = Request(f'https://api.ipsw.me/v4/keys/ipsw/{device}/{version}', headers={'Accept': 'application/json'})
+    response_body = urlopen(url).read()
+    print(response_body)
+    # TODO If keys are not found, MAYBE use iphonewiki for keys, or even just use iphonewiki only because from before
+    # some keys are still missing on ipsw.me Maybe even implement a function that grabs keys with GID from ipwndfu
+    # and open a page with proper info. Create a local file or print out contents and just copy and paste to url.
