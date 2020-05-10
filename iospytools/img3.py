6e1bc0fe584a960a11b4d4ae80c9d1e372b62ae5
Merculous <24739823+Merculous@users.noreply.github.com>
2020-05-09 19:02:14 -0700

    Fix yeet to look better in cli and add wip usb and img3 stuff

--- iospytools/img3.py
+++ iospytools/img3.py
@@ -1,5 +1,8 @@
-import re
-
+import struct
+import sys
+import os
+import math
+import humanize
 
 """
 typedef struct img3File {
@@ -11,7 +14,7 @@ typedef struct img3File {
                           // the start of the RSA signature (SHSH section)
     uint32_t ident;       // identifier of image, used when bootrom is parsing images
                           // list to find LLB (illb), LLB parsing it to find iBoot (ibot),
-                          // etc. 
+                          // etc.
     img3Tag  tags[];      // continues until end of file
 };
 
@@ -38,7 +41,7 @@ TYPE: Type of image, should contain the same string as the header's ident
 DATA: Real content of the file
 NONC: Nonce used when file was signed.
 CEPO: Chip epoch
-OVRD: Overide thingy for demotion - axi0mX
+OVRD: Override thingy for demotion - axi0mX
 RAND:
 SALT:
 """
@@ -49,6 +52,9 @@ class IMG3(object):
         super().__init__()
 
         self.file = file
+        self.totalsize = os.path.getsize(self.file)
+        print('Name: {}'.format(os.path.basename(self.file)))
+        print('Size: {}'.format(humanize.naturalsize(self.totalsize)))
 
         """
 
@@ -56,16 +62,42 @@ class IMG3(object):
 
         """
 
-    # Data is reversed (little-endian) IMG3 magic will be "3gmI". Need a good way to take a string, convert to hex or whatever, and reverse it.
-
     def parseImage(self):
         # Return tags, iBoot version, and other info about an img3
 
-        info = {
-
-        }
+        # Can return a dict with tags and their position, maybe even make this somewhat of a new way of parsing img3?
+        # info = {
+        #
+        # }
 
         with open(self.file, 'rb') as f:
-            data = f.read()
-            print(data)
-        f.close()
+            img3_start = f.read(4)
+            new = img3_start.decode('utf-8')[::-1]
+            if new == 'Img3':
+                # To grab proper tag type, we must not only get the type tag, which will give the correct tag, ident will also give the same value
+                # which means, we have two sources. So, we should not base the file off of one, to increase likelihood that whatever is parsed is done
+                # with as much precision as possible. At least from my point of view, that seems reasonable enough, because with built iBoot however...
+
+                # Get type of img3
+
+                tags = ['rdsk', 'ibss', 'ibec', 'ibot', 'illb', 'dtre', 'logo', 'chg0',
+                        'chg1', 'batF', 'bat0', 'bat1', 'glyC', 'glyP', 'nsrv', 'recm']
+
+                #data = struct.unpack('<I', f.read())
+                # print(type(data))
+                data = f.read()
+
+                for tag in tags:
+                    tag = tag.encode()[::-1]  # Convert to bytes, and reverse the bytes due to little-endian
+                    count = 1
+                    duplicated = list()
+                    if tag in data:
+                        count += 1
+                        duplicated.append(tag)
+                        duplicated.append(count)
+
+                    print('Tag {} repeated {} times!'.format(
+                        duplicated[0], duplicated[1]))
+
+            else:
+                raise ValueError('Not an img3 file')
--- iospytools/usb.py
+++ iospytools/usb.py
@@ -1,5 +1,5 @@
 try:
-    import usb.core
+    import usb
 except ImportError as error:
     print('Oof, got error:', error)
     raise
@@ -9,4 +9,42 @@ class USB:
     def __init__(self):
         super().__init__()
 
-        self.dev = usb.core.find()
+        # Some stuff with WTF, maybe we can force DFU somehow?
+
+        # 05ac:1260 Nano 2G regular/disk mode
+
+        # ffff:8642 Nano 2G iBugger Loader
+
+        # dev.idVendor == 0x05ac and dev.idProduct == 0x1220 Bootrom DFU
+
+        # dev.idVendor == 0x05ac and dev.idProduct == 0x1240 Nano 2G NOR DFU (WTF I think/SoftDFU)
+
+        # 05ac:12a8 Normal mode iPhone 5S
+
+        # 05ac:1227 DFU mode iPhone 5S
+
+        # 05ac:1281 Recovery mode iPhone 5S
+
+        # Uploading /home/merculous/Documents/ibugger/ibugger/core-2.bin to 0x22000000.... done
+        # Passing control to code at 0x22000020... done
+        # Connected to iBugger Core v0.1.1 on iPod Nano 2G, USB version 00.01
+
+        # TODO Add Pwnage 2.0 support. Just uploading a malformed certificate/file
+
+    def getDevices(self):
+        pass
+
+    def sendPacket(self):
+        pass
+
+    def unpackPacket(self, packet):
+        pass
+
+    def debugNano2G(self):
+        pass
+
+    def sendCommand(self, command):
+        pass
+
+    def enterRecovery(self):
+        pass
