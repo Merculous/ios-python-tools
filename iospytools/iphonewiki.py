cd62708c61632536c14141dd8366eb0a4ef3c272
Merculous <24739823+Merculous@users.noreply.github.com>
2020-06-21 22:53:53 -0700

    Update key grabbing

--- iospytools/iphonewiki.py
+++ iospytools/iphonewiki.py
@@ -1,10 +1,7 @@
-import os
-import json
 from urllib.request import urlopen
 
 try:
     from .ipswapi import API
-    from .manifest import BuildManifest
     from .template import Template
 except ImportError:
     raise
@@ -25,33 +22,10 @@ class iPhoneWiki(object):
         """
 
     # TODO Add OTA compatibility, allow single file grabbing
-    def getWikiKeys(self, save=False, file=False):
-        try:
-            api = API(self.device, self.version)
-            buildid = api.iOSToBuildid()
-        except ConnectionError:
-            print('Got an Apple or ipsw.me connection error!')
-            raise
-
-        template = Template()
-
-        path = 'BuildManifest.plist'
-
-        if os.path.exists(path):  # Also, just in case if the user terminated
-            # So we don't have a leftover manifest that isn't the same device and or iOS
-            os.remove(path)
-
-        try:
-            api.downloadManifest()  # To keep data "constant" we need to download every time
-        except ConnectionError:
-            print('Failed to download build manifest!')
-            raise
-
-        build_manifest = BuildManifest()
-        # Get BuildManiest.plist codename, filenames, and file paths.
-        manifest_data = build_manifest.extractData()
-        codename = manifest_data['codename']
-
+    def getWikiKeys(self):
+        api = API(self.device, self.version)
+        buildid = api.iOSToBuildid()
+        codename = api.getCodename()
         wikiUrl = 'https://www.theiphonewiki.com/w/index.php?title={}_{}_({})&action=edit'.format(
             codename, buildid, self.device)
         try:
@@ -60,18 +34,8 @@ class iPhoneWiki(object):
             print('Failed to request data from iPhoneWiki!')
             raise
         else:
-            data = template.parseTemplate(request)
-            if save:  # FIXME
-                json_path = '{}_{}_{}.json'.format(
-                    self.device, self.version, buildid)
-                if path in os.listdir(os.getcwd()):
-                    os.remove(path)
-                with open(json_path, 'w') as f:
-                    print('Writing contents to file...')
-                    f.write(json.dumps(data))
-                f.close()
-            else:
-                return data
+            template = Template()
+            return template.parseTemplate(request)
 
     # TODO Maybe have it open an html file, the page to upload keys, importing template into the page, just needing to press "upload"
 
