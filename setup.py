0611b9e97617c84707fb8e9adc3e78233c29facf
Merculous <24739823+Merculous@users.noreply.github.com>
2020-08-23 21:50:32 -0700

    Oops, forgot to bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setup(
     name="iospytools",
-    version="1.0.5",
+    version="1.0.6",
     author="Merculous",
     author_email="vycemerculous@gmail.com",
     description="provides useful tools/commands which are used in iOS research",
