19380f9c38b6aa3a17b6e6c6048650242fc8c4f7
Merculous <24739823+Merculous@users.noreply.github.com>
2022-02-25 15:51:00 -0800

    Start new wiki code

--- iospytools/iphonewiki.py
+++ iospytools/iphonewiki.py
@@ -1,21 +1,52 @@
-from urllib.request import urlopen
 
-from .template import Template
+import re
+from .remote import downloadFromURL, requestFromURL
 
 
 class Wiki:
-    def __init__(self, device: str, buildid: str, codename: str) -> None:
-        self.device = device
-        self.buildid = buildid
-        self.codename = codename
+    def __init__(self) -> None:
+        self.info = {}
 
-        self.api = 'https://www.theiphonewiki.com/w/api.php'
+    def readModels(self):
+        url = 'https://www.theiphonewiki.com/w/index.php?title=Models&action=edit'
+        return requestFromURL(url, True).decode().splitlines()
 
-    # TODO Add OTA and Beta support, if I specifially need to specify it
+    def extractTemplate(self, text: list):
+        template = {}
 
-    def requestKeys(self) -> list:
-        url = 'https://www.theiphonewiki.com/w/index.php?title={}_{}_({})&action=edit'.format(
-            self.codename, self.buildid, self.device)
-        r = urlopen(url).read().decode()
-        t = Template(r)
-        return t.parse()
+        devices = (
+            re.compile(r'apple tv', re.IGNORECASE),
+            re.compile(r'apple watch', re.IGNORECASE),
+            re.compile(r'ipad', re.IGNORECASE),
+            re.compile(r'ipod', re.IGNORECASE),
+            re.compile(r'iphone', re.IGNORECASE)
+        )
+
+        count = 0
+
+        for line in text:
+
+            if line.startswith('== ') and line.endswith(' =='):
+                for device in devices:
+                    if device.search(line):
+                        section = line[5:-5].split('|')
+                        template[section[1]] = {
+                            'title': section[0],
+                            'start': count
+                        }
+
+            # FIXME Below is ugly
+
+            if line == '|}':
+                if template:
+                    template[section[1]]['end'] = count + 1
+
+                    template[section[1]]['length'] = template[section[1]
+                                                              ]['end'] - template[section[1]]['start']
+
+                    template[section[1]]['data'] = text[template[section[1]]
+                                                        ['start']:template[section[1]]['end']]
+
+            count += 1
+
+        return template
