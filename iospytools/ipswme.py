de1ab6ebe75d606f74f67ea98e7f994860465baa
Merculous <24739823+Merculous@users.noreply.github.com>
2022-04-26 17:28:05 -0700

    Some more stuff

--- iospytools/ipswme.py
+++ iospytools/ipswme.py
@@ -1,6 +1,8 @@
 
 import json
 
+from enquiries import choose
+
 from .remote import getURLData
 
 
@@ -44,7 +46,37 @@ class IPSWAPI:
                 iOS = firmware['version']
                 buildid = firmware['buildid']
                 if self.version == iOS:
-                    matches.append(buildid)
+                    if buildid not in matches:
+                        matches.append(buildid)
             return matches
         else:
-            raise ValueError('No iOS version wass passed!')
+            raise ValueError('No device or iOS version was passed!')
+
+    async def getArchiveURL(self) -> str:
+        if self.device and self.version:
+            info = await self.getDeviceInfo()
+            firmwares = info['firmwares']
+            buildids = await self.iOSToBuildid()
+            if len(buildids) == 1:
+                for firmware in firmwares:
+                    if firmware['buildid'] == buildids[0]:
+                        return firmware['url']
+            else:
+                prompt = 'Please select which version you\'d like to use.'
+                choice = choose(prompt, buildids)
+                for firmware in firmwares:
+                    if firmware['buildid'] == choice:
+                        return firmware['url']
+
+    async def getSignedVersions(self) -> list:
+        if self.device:
+            info = await self.getDeviceInfo()
+            firmwares = info['firmwares']
+            signed = []
+            for firmware in firmwares:
+                if firmware['signed']:
+                    tmp = (firmware['version'], firmware['buildid'])
+                    if tmp not in signed:
+                        signed.append(tmp)
+        else:
+            raise ValueError('No device was passed!')
