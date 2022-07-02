19c30fd32e6a3fdaed99ca351545b81a06436213
Merculous <24739823+Merculous@users.noreply.github.com>
2022-07-01 19:42:54 -0500

    Add some archive functions

--- iospytools/archive.py
+++ iospytools/archive.py
@@ -1,21 +1,35 @@
 
-import os
-import zipfile
-from typing import Generator
-
+from hashlib import sha256
+from zipfile import ZipFile
 
 class Archive:
     def __init__(self, path: str) -> None:
         self.path = path
 
-    def info(self) -> dict:
-        """Get a dict of the contents of a zip archive with info about each path"""
-        if zipfile.is_zipfile(self.path):
-            with zipfile.ZipFile(self.path) as f:
-                return f.NameToInfo
-        else:
-            raise zipfile.error(f'{self.path} is not a zip archive!')
+    def getSHA256(self):
+        with open(self.path, 'rb') as f:
+            return sha256(f.read()).hexdigest()
+        
+    def listContents(self):
+        with ZipFile(self.path) as f:
+            contents = f.filelist
+            for content in contents:
+                print(content.filename)
 
-    def listContents(self) -> Generator:
-        """Extract splitted paths from a zip archive"""
-        return (os.path.split(p) for p in self.info())
+    def createArchive(self, contents):
+        pass
+
+
+    def extract(self, member=None, out_path=None):
+        if member:
+            with ZipFile(self.path) as f:
+                if out_path:
+                    f.extract(member, out_path)
+                else:
+                    f.extract(member)
+        else:
+            with ZipFile(self.path) as f:
+                if out_path:
+                    f.extractall(out_path)
+                else:
+                    f.extractall()
